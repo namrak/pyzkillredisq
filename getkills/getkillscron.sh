da0c1fe02945f9da13795609a2f2debd95a0e74a
namrak <namrak@gmail.com>
2017-05-07 12:54:37 -0600

    fixed typo

--- getkills/getkillscron.sh
+++ getkills/getkillscron.sh
@@ -2,6 +2,6 @@
 
 # shell script to start gathering killmails via crontab
 # recommend a line similar to the following to run every hour:
-# 00 * * * * env DISPLAY=:0 /usr/bin/xterm -e /home/<username>/pyzkillredisq/getkills/crongetkills.sh
+# 00 * * * * env DISPLAY=:0 /usr/bin/xterm -e /home/<username>/pyzkillredisq/getkills/getkillscron.sh
 cd /home/<username>/pyzkillredisq/
 python3.6 getkills/getkills.py
